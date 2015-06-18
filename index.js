d94803c97f8aeb8b385c7e35745647c21311cf06
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2015-06-18 16:37:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,6 +14,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(app.get('port'), function(){
   console.log('listening on *:3000');
 });
